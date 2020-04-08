5a90a5a7cc362c5be7723e66810e9ff303016fa7
Elise <elisemykle@hotmail.com>
2020-04-08 21:08:07 +0200

    Today's work done

---
+++ src/components/home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Home() {
+    return (
+        <h1>Portfolio</h1>
+            //  <div className="her-image">
+            //   <div className="hero-text">
+            //   <img src="img/elisee.png" alt="name" class="responsive">
+            //   <p id="herotext">PORTFOLIO</p>
+            //   </div>
+            // </div>
+    );
+}
+
+export default Home;
