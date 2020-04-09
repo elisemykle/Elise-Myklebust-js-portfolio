990a8ce47f47a765e779ca7eb6f5c65fcd09f4cf
Elise <elisemykle@hotmail.com>
2020-04-09 22:43:37 +0200

    Set up scss fiels

--- src/App.js
+++ src/App.js
@@ -1,11 +1,56 @@
 import React from 'react';
-import './css/App.scss';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Layout from './Layout.js';
+import './css/style.scss';
+import {
+  BrowserRouter as Router,
+  NavLink,
+  Route,
+  Switch
+} from "react-router-dom";
+import {Container, Nav, Navbar} from "react-bootstrap";
+import Home from "./components/home/Home";
+import About from "./components/about/About";
+import Contact from "./components/contact/Contact";
+import Cv from "./components/cv/Cv";
+import logo from "./image/icon.png"
 
 function App() {
   return (
-      <Layout />
+      <div className="frontpage">
+        <Router className="Layout">
+          <Navbar bg="light" variant="light" expand="lg">
+            <NavLink to="/" exact>
+                <Navbar.Brand>
+                    <img src={logo} alt="my logo"/>
+                </Navbar.Brand>
+            </NavLink>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="mr-auto">
+                <NavLink to="/" exact className="nav__link">HOME</NavLink>
+                <NavLink to="/About" exact className="nav__link">ABOUT</NavLink>
+                <NavLink to="/Cv" exact className="nav__link">CV</NavLink>
+                <NavLink to="/Contact" className="nav__link">CONTACT</NavLink>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+          <Container>
+            <Switch>
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route exact path="/about">
+                <About />
+              </Route>
+              <Route exact path="/contact">
+                <Contact/>
+              </Route>
+              <Route exact path="/cv">
+                <Cv/>
+              </Route>
+            </Switch>
+          </Container>
+        </Router>
+      </div>
   );
 }
 
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import heroImg from "../../image/heroimage.png"
 
 export default function Home(props) {
     return (
         <div className="page page__home">
             <h1 className="page__h1">Portfolio</h1>
-            <img src="./../../image/heroimage.png" alt="heroimage"/>
+            <img src={heroImg} alt="heroimage"/>
         </div>
     )
 }
