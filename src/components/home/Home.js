572395b6a4c963d0f5a5283b580811a39956f75a
Elise Myklebust <elisemykle@hotmail.com>
2020-04-12 21:58:35 +0200

    Text on project images

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -19,22 +19,22 @@ export default function Home(props) {
         <Container id="content">
             <h1 className="home__h1">My Projects...</h1>
           <Row>
-            <Col className="home__projects" xs={12} md={6} lg={3}><Image src={kortImg} className="home__kort" alt="kort img" fluid/></Col>
-            <Col xs={12} md={6} lg={3}><Image src={koppenImg} className="home__koppen" alt="koppen img" fluid/></Col>
-            <Col xs={12} md={6} lg={3}><Image src={magasinImg} className="home__magasin" alt="magasin img" fluid/></Col>
-            <Col xs={12} md={6} lg={3}><Image src={eliseImg} className="home__elise" alt="elise img" fluid/></Col>
+            <Col className="home__projects" xs={12} md={6} lg={3}><Image src={kortImg} className="home__kort" alt="kort img" fluid/> <p className="home__centered">centered</p></Col>
+            <Col className="home__projects" xs={12} md={6} lg={3}><Image src={koppenImg} className="home__koppen" alt="koppen img" fluid/></Col>
+            <Col className="home__projects" xs={12} md={6} lg={3}><Image src={magasinImg} className="home__magasin" alt="magasin img" fluid/></Col>
+            <Col className="home__projects" xs={12} md={6} lg={3}><Image src={eliseImg} className="home__elise" alt="elise img" fluid/></Col>
           </Row>
           <Row>
-            <Col xs={12} md={6} lg={3}><Image src={gymImg} className="home__gym" alt="gym img" fluid/></Col>
-            <Col xs={12} md={6} lg={3}><Image src={pizzaImg} className="home__pizza" alt="pizza img" fluid/></Col>
-            <Col xs={12} md={6} lg={3}><Image src={appImg} className="home__app" alt="app img" fluid/></Col>
-            <Col xs={12} md={6} lg={3}><Image src={visaImg} className="home__visa" alt="visa img" fluid/></Col>
+            <Col className="home__projects" xs={12} md={6} lg={3}><Image src={gymImg} className="home__gym" alt="gym img" fluid/></Col>
+            <Col className="home__projects" xs={12} md={6} lg={3}><Image src={pizzaImg} className="home__pizza" alt="pizza img" fluid/></Col>
+            <Col className="home__projects" xs={12} md={6} lg={3}><Image src={appImg} className="home__app" alt="app img" fluid/></Col>
+            <Col className="home__projects"xs={12} md={6} lg={3}><Image src={visaImg} className="home__visa" alt="visa img" fluid/></Col>
           </Row>
           <Row>
-            <Col xs={12} md={6} lg={3}><Image src={flavourImg} className="home__flavour" alt="flavour img" fluid/></Col>
-            <Col xs={12} md={6} lg={3}><Image src={stickerImg} className="home__sticker" alt="sticker img" fluid/></Col>
-            <Col xs={12} md={6} lg={3}><Image src={orangeImg} className="home__orange" alt="orange img" fluid/></Col>
-            <Col xs={12} md={6} lg={3}><Image src={barekstenImg} className="home__bareksten" alt="bareksten img" fluid/></Col>
+            <Col className="home__projects" xs={12} md={6} lg={3}><Image src={flavourImg} className="home__flavour" alt="flavour img" fluid/></Col>
+            <Col className="home__projects" xs={12} md={6} lg={3}><Image src={stickerImg} className="home__sticker" alt="sticker img" fluid/></Col>
+            <Col className="home__projects" xs={12} md={6} lg={3}><Image src={orangeImg} className="home__orange" alt="orange img" fluid/></Col>
+            <Col className="home__projects" xs={12} md={6} lg={3}><Image src={barekstenImg} className="home__bareksten" alt="bareksten img" fluid/></Col>
           </Row>
         </Container>
     )
