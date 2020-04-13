755478288fe4854e0b15ace809b6e85790d40dba
Elise Myklebust <elisemykle@hotmail.com>
2020-04-14 01:32:59 +0200

    Changes images

---
+++ src/components/projects/Project.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Project(props) {
+    return (
+        <div className="Project page__home">
+            <h1 className="Project__h1">Carbon Solutions</h1>
+            <p>This assignment was to create a new branding identity for the company - Carbon Solutions. The company wanted a appealing design that would attract new customers, investors and the government. The target group should be carefully considered and the proposed design should reflect this.</p>
+        </div>
+    )
+}
--- src/components/projects/Zerogravity.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Zerogravity(props) {
-    return (
-        <div className="Zerogravity page__home">
-            <h1 className="Zerogravity__h1">Zero gravity</h1>
-        </div>
-    )
-}
