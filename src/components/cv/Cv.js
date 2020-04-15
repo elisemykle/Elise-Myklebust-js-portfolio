34dbb0d230fce3124daf64db0c075e060dd0ed0f
Elise Myklebust <elisemykle@hotmail.com>
2020-04-16 01:18:53 +0200

    Started on timeline

--- src/components/cv/Cv.js
+++ src/components/cv/Cv.js
@@ -2,8 +2,50 @@ import React from 'react';
 
 export default function Cv(props) {
     return (
-        <div className="cv__heading">
-          <h1 className="cv__h1">Cv - work experience</h1>
+        <div className="timeline">
+            <h1 className="cv__h1">Cv - work experiences</h1>
+          <div className="container__right">
+            <div className="cv__content">
+                <h2 className="timeline__h2">2020</h2>
+                <p className="timeline__text">Frontend utvikling - Noroff school of technology and digital media | 2018 - 2017</p>
+            </div>
           </div>
+
+          <div className="container__left">
+            <div className="cv__content">
+                <h2 className="timeline__h2">2019</h2>
+                <p className="timeline__text">Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</p>
+            </div>
+          </div>
+
+          <div className="container__right">
+            <div className="cv__content">
+                <h2 className="timeline__h2">2018</h2>
+                <p className="timeline__text">Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</p>
+            </div>
+          </div>
+
+          <div className="container__left">
+            <div className="cv__content">
+                <h2 className="timeline__h2">2017</h2>
+                <p className="timeline__text">Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</p>
+            </div>
+          </div>
+
+          <div className="container__right">
+            <div className="cv__content">
+                <h2 className="timeline__h2">2016</h2>
+                <p className="timeline__text">Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</p>
+            </div>
+          </div>
+
+          <div className="container__right">
+            <div className="cv__content">
+                <h2 className="timeline__h2">2015</h2>
+                <p className="timeline__text">Grafisk design - Høyskolen Kristiania Bergen | 2015 - 2017</p>
+            </div>
+          </div>
+        </div>
+
     )
 }
