6c2c798075ce6588cdf7414f8177d7718433d8dd
Elise Myklebust <elisemykle@hotmail.com>
2020-04-15 22:58:24 +0200

    done some changes with the contact form

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -32,26 +32,26 @@ const schema = yup.object().shape({
                 <div className="contact__contact--page">
                     <h1 className="contact__h1">Oh, hello there...</h1>
                 </div>
-                <Form.Label>First name</Form.Label>
+                <Form.Label className="form__name">First name</Form.Label>
                 <Form.Control name="firstname" placeholder="Enter your first name" ref={register}/>
                 {errors.firstname && <p>Please enter minimum 2 characters.</p>}
             </Form.Group>
             <Form.Group>
-                <Form.Label>Last name</Form.Label>
+                <Form.Label className="form__name">Last name</Form.Label>
                 <Form.Control name="lastname" placeholder="Enter your last name" ref={register}/>
                 {errors.lastname && <p>Please enter minimum 2 characters.</p>}
             </Form.Group>
 			<Form.Group>
-                <Form.Label>Email adress</Form.Label>
+                <Form.Label className="form__name">Email adress</Form.Label>
                 <Form.Control name="emailadress" placeholder="Enter your email" ref={register}/>
                 {errors.emailadress && <p>Please enter in a valid email format.</p>}
             </Form.Group>
 			<Form.Group>
-				<Form.Label>Message</Form.Label>
-				<Form.Control name="message" placeholder="Type your message here..." ref={register}/>
+				<Form.Label className="form__name">Message</Form.Label>
+				<Form.Control className="message__form" as="textarea" name="message" placeholder="please enter your message here" ref={register}/>
 				{errors.message && <p>Please enter in minimum 10 characters.</p>}
 			</Form.Group>
-            <Button type="submit">Fly away</Button>
+            <Button className="contact__button" type="submit">Send</Button>
         </Form>
         )
 }
