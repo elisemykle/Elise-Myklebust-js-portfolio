79fe3704750f2c31cad07dbe5a690f9afc9f1fa9
Elise Myklebust <elisemykle@hotmail.com>
2020-04-17 21:31:51 +0200

    validation done

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -5,15 +5,18 @@ import Form from "react-bootstrap/Form";
 import * as yup from "yup";
 
 const schema = yup.object().shape({
-	firstname: yup.string().required("Required, minimum 2 characters"),
+	firstname: yup
+        .string()
+		.required()
+		.min(2, "Required, minimum 2 characters"),
 	lastname: yup
         .string()
 		.required()
-		.min(8, "Required, minimum 2 characters"),
+		.min(2, "Required, minimum 2 characters"),
 	emailadress: yup
 		.string()
 		.required()
-		.min(8, "Required, must be in a valid email format"),
+		.min("Required, must be in a valid email format"),
 	message: yup
 		.string()
 		.required()
@@ -34,22 +37,22 @@ const schema = yup.object().shape({
                 </div>
                 <Form.Label className="form__label">First name</Form.Label>
                 <Form.Control name="firstname" placeholder="Enter your first name" ref={register}/>
-                {errors.firstname && <p>Please enter minimum 2 characters.</p>}
+                {errors.firstname && <p className="error__message">Please enter minimum 2 characters.</p>}
             </Form.Group>
             <Form.Group>
                 <Form.Label className="form__label">Last name</Form.Label>
                 <Form.Control name="lastname" placeholder="Enter your last name" ref={register}/>
-                {errors.lastname && <p>Please enter minimum 2 characters.</p>}
+                {errors.lastname && <p className="error__message">Please enter minimum 2 characters.</p>}
             </Form.Group>
 			<Form.Group>
                 <Form.Label className="form__label">Email adress</Form.Label>
                 <Form.Control name="emailadress" placeholder="Enter your email" ref={register}/>
-                {errors.emailadress && <p>Please enter in a valid email format.</p>}
+                {errors.emailadress && <p className="error__message">Please enter in a valid email format.</p>}
             </Form.Group>
 			<Form.Group>
 				<Form.Label className="form__label">Message</Form.Label>
 				<Form.Control className="message__form" as="textarea" name="message" placeholder="Please enter your message here..." ref={register}/>
-				{errors.message && <p>Please enter in minimum 10 characters.</p>}
+				{errors.message && <p className="error__message">Please enter in minimum 10 characters.</p>}
 			</Form.Group>
             <Button className="contact__button" type="submit">Send</Button>
         </Form>
