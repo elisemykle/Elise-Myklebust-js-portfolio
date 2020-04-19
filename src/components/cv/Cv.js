ce3611fd1476e0c9475b3f9f910e4dbb09311991
Elise Myklebust <elisemykle@hotmail.com>
2020-04-19 13:27:43 +0200

    timeline undertext

--- src/components/cv/Cv.js
+++ src/components/cv/Cv.js
@@ -12,11 +12,11 @@ export default function Cv(props) {
               <div className="timeline__panel">
                 <div className="timeline__heading">
                   <h4 className="timeline__title">Technical skills</h4>
-                    <p><small className="text__muted">Here are a list of my skills </small></p>
+                    <p className="timeline__undertext">Here are a list of my skills</p>
                 </div>
                 <div className="timeline__body">
-                  <p>Graphic design: Illustrator, Photoshop , Indesign, Lightroom, Adobe xd, Wordpress.
-                      Programming languages: Html, Css, Javascript, React.
+                  <p>Graphic design: Illustrator, Photoshop , Indesign, Lightroom, Adobe xd and Wordpress.
+                      Programming languages: Html, Css, Scss, Javascript and React.
                   </p>
                 </div>
               </div>
@@ -25,8 +25,8 @@ export default function Cv(props) {
             <li className="timeline__inverted">
               <div className="timeline__panel">
                 <div className="timeline__heading">
-                  <h4 className="timeline__title">Høyskolen Kristiania</h4>
-                    <p><small className="text__muted">2020 </small></p>
+                  <h4 className="timeline__title">Noroff School Of Technology And Digital Media </h4>
+                    <p className="timeline__undertext">2018 - 2020 Front-end development </p>
                 </div>
                 <div className="timeline__body">
                   <p>Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis. Mé faiz elementum girarzis, nisi eros vermeio, in elementis mé pra quem é amistosis quis leo. Manduma pindureta quium dia nois paga. Sapien in monti palavris qui num significa nadis i pareci latim. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.</p>
@@ -37,8 +37,8 @@ export default function Cv(props) {
             <li>
               <div className="timeline__panel">
                 <div className="timeline__heading">
-                  <h4 className="timeline__title">Noroff School Of Technology And Digital Media </h4>
-                  <p><small className="text__muted">2020 </small></p>
+                  <h4 className="timeline__title">Høyskolen Kristiania</h4>
+                  <p className="timeline__undertext">2015 - 2017 Graphic design</p>
                 </div>
                 <div className="timeline__body">
                   <p>Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis. Mé faiz elementum girarzis, nisi eros vermeio, in elementis mé pra quem é amistosis quis leo. Manduma pindureta quium dia nois paga. Sapien in monti palavris qui num significa nadis i pareci latim. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.</p>
@@ -49,8 +49,8 @@ export default function Cv(props) {
             <li className="timeline__inverted">
               <div className="timeline__panel">
                 <div className="timeline__heading">
-                  <h4 className="timeline__title">Borgund Vidergående</h4>
-                  <p><small className="text__muted">2020 </small></p>
+                  <h4 className="timeline__title">Volda videregående</h4>
+                  <p className="timeline__undertext">2012 - 2013 General university admissions certification</p>
                 </div>
                 <div className="timeline__body">
                   <p>Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis. Mé faiz elementum girarzis, nisi eros vermeio, in elementis mé pra quem é amistosis quis leo. Manduma pindureta quium dia nois paga. Sapien in monti palavris qui num significa nadis i pareci latim. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.</p>
@@ -59,22 +59,22 @@ export default function Cv(props) {
             </li>
 
             <li>
+              <div className="timeline__panel">
+                <div className="timeline__heading">
+                  <h4 className="timeline__title">Borgund videregående skole</h4>
+                  <p className="timeline__undertext">2011 - 2012 Hairdresser school </p>
+                </div>
+                <div className="timeline__body">
+                  <p>Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis. Mé faiz elementum girarzis, nisi eros vermeio, in elementis mé pra quem é amistosis quis leo. Manduma pindureta quium dia nois paga. Sapien in monti palavris qui num significa nadis i pareci latim. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.</p>
+                </div>
+              </div>
+            </li>
+
+            <li className="timeline__inverted">
               <div className="timeline__panel">
                 <div className="timeline__heading">
                   <h4 className="timeline__title">Ørsta Videregående</h4>
-                  <p><small className="text__muted">2020 </small></p>
-                </div>
-                <div className="timeline__body">
-                  <p>Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis. Mé faiz elementum girarzis, nisi eros vermeio, in elementis mé pra quem é amistosis quis leo. Manduma pindureta quium dia nois paga. Sapien in monti palavris qui num significa nadis i pareci latim. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.</p>
-                </div>
-              </div>
-            </li>
-
-            <li className="timeline__inverted">
-              <div className="timeline__panel">
-                <div className="timeline__heading">
-                  <h4 className="timeline__title">Volda Videregående</h4>
-                  <p><small className="text__muted">2020 </small></p>
+                  <p className="timeline__undertext">2010 - 2011 Design and craftsmanship</p>
                 </div>
                 <div className="timeline__body">
                   <p>Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis. Mé faiz elementum girarzis, nisi eros vermeio, in elementis mé pra quem é amistosis quis leo. Manduma pindureta quium dia nois paga. Sapien in monti palavris qui num significa nadis i pareci latim. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.</p>
